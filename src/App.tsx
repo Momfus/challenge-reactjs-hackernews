024b1322457a51845b67899539297cfd7822485b
Julian Muñoz Velázquez <momfus@outlook.com>
2023-11-14 23:44:17 -0300

    Test commit to see the problems with netifly

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from "react";
 import "./App.css";
 import { HackerNewsProvider } from "./Contexts/hackerNewsContext";
 import { BrowserRouter } from "react-router-dom";
-import Header from "./Components/Header/header";
+// import Header from "./Components/Header/header";
+
 import { AppRoutes } from "./Routes/routes";
 
 function App() {
   return (
     <HackerNewsProvider>
       <BrowserRouter>
-        <Header title="Hacker News" />
+        {/* <Header title="Hacker News" /> */}
         <AppRoutes></AppRoutes>
       </BrowserRouter>
     </HackerNewsProvider>

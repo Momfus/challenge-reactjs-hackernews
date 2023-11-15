67c6c9abd0018b8da09048d13ac042f79b200f30
Julian Muñoz Velázquez <momfus@outlook.com>
2023-11-14 23:51:02 -0300

    Header index name change

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import "./App.css";
 import { HackerNewsProvider } from "./Contexts/hackerNewsContext";
 import { BrowserRouter } from "react-router-dom";
-// import Header from "./Components/Header/header";
 
 import { AppRoutes } from "./Routes/routes";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <HackerNewsProvider>
       <BrowserRouter>
-        {/* <Header title="Hacker News" /> */}
+        <Header title="Hacker News" />
         <AppRoutes></AppRoutes>
       </BrowserRouter>
     </HackerNewsProvider>
